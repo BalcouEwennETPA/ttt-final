899f61ca415c9c37111213f891a577354d32c3c9
BalcouEwennETPA <ewennbalcou10@gmail.com>
2020-12-02 14:09:24 +0100

    Update ttt final.py

--- ttt final.py
+++ ttt final.py
@@ -20,3 +20,23 @@ def afficher_grille():
 	for i in range(3):
 		print(grille[i*3],grille[i*3+1],grille[i*3+2])
 		
+
+def joueur_O_X(joueur_f):
+	if(joueur_f == "X"):
+		joueur = "O"
+	else:
+		joueur = "X"
+	return joueur
+
+def win(victoire):
+	for i in range(3):
+
+		if (grille[i*3] == grille[i*3+1] == grille[i*3+2] != "*"):
+			victoire = 1
+		if (grille[i] == grille[i+3] == grille[i+6] != "*"):
+			victoire = 1
+
+	for i in range(2):
+		if(grille[i+i] == grille[i+4-i] == grille[i+8-(i*3)] != "*"):
+			victoire = 1
+	return victoire
